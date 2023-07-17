8007457fdaf398be3b3bd4477f58fda207b7262f
sonukuldeep <sonukuldeep@outlook.com>
2023-07-17 10:57:32 +0530

    lesson 447

--- src/frontend/src/index.jsx
+++ src/frontend/src/index.jsx
@@ -1,13 +1,27 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from './components/App';
+import { AuthClient } from '@dfinity/auth-client';
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+const authClient = await AuthClient.create()
 
+if (await authClient.isAuthenticated()) {
+  handleAuthentication(authClient)
+} else
+  await authClient.login({
+    identityProvider: "https://identity.ic0.app/#authorize",
+    maxTimeToLive: BigInt(60 * 60 * 1000 * 1000 * 1000),
+    onSuccess: () => {
+      handleAuthentication(authClient)
+    }
+  })
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+async function handleAuthentication(authClient) {
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}

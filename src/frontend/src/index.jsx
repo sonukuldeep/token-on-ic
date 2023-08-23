72bdc217399f4e8d354bf54288b49cd04d7baee1
Kuldeep kumar <57728165+sonukuldeep@users.noreply.github.com>
2023-08-23 11:34:20 +0530

    Big update index.jsx
    So I'm deploying a local identity provider canister,on mainnet change identity provider to mainnet canister

--- src/frontend/src/index.jsx
+++ src/frontend/src/index.jsx
@@ -11,19 +11,20 @@ if (await authClient.isAuthenticated()) {
   handleAuthentication(authClient)
 } else
   await authClient.login({
-    identityProvider: "https://identity.ic0.app/#authorize",
-    maxTimeToLive: BigInt( 60 * 1000 * 1000 * 1000),
+    // identity provider address is pointing to localhost at the moment
+    // when deploying to mainnet change to 
+    // identityProvider: "https://identity.ic0.app/#authorize",
+    identityProvider: "http://127.0.0.1:4943/?canisterId=be2us-64aaa-aaaaa-qaabq-cai",
+    maxTimeToLive: BigInt(60 * 1000 * 1000 * 1000),
     onSuccess: () => {
       handleAuthentication(authClient)
     }
   })
 
 async function handleAuthentication(authClient) {
-  const { _principal: userPrincipal } = await authClient.getIdentity()
-
   root.render(
     <StrictMode>
-      <App loggedInPrincipal={userPrincipal.toString()} />
+      <App />
     </StrictMode>
   );
 }
